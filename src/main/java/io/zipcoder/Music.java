27597cf2c7148470169da0268f976bca0b82afbd
c-poteat <nellp@mail.com>
2021-11-23 14:27:34 -0500

    Updated lab

--- src/main/java/io/zipcoder/Music.java
+++ src/main/java/io/zipcoder/Music.java
@@ -1,14 +1,26 @@
 package io.zipcoder;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
 public class Music {
 
     private String[] playList;
 
-    public Music(String[] playList){
+    public Music(String[] playList) {
         this.playList = playList;
     }
 
-    public Integer selection(Integer startIndex, String selection){
-        return null;
+    public Integer selection(Integer startIndex, String selection) {
+        List<String> stringMatch = new ArrayList<String>(Arrays.asList(playList));
+        startIndex = 1;
+        for (int i = startIndex; i <= stringMatch.size(); i++) {
+            if (stringMatch.contains(selection)) {
+
+            }
+        }
+        return stringMatch.indexOf(selection) - startIndex;
     }
 }
+
